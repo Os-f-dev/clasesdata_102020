f11222fb46b6b1811f0cefee45b3543d216e75b2
Os-f-dev <oscar.fuentes@ironhack.com>
2020-10-28 13:11:19 -0600

    w3_d3_correct

--- scrap_price_tm.py
+++ scrap_price_tm.py
@@ -4,24 +4,31 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
+
+#Se configura driver para poder manejar mejor el navegador
 caps = DesiredCapabilities().FIREFOX
 caps["marionette"] = True
-driver = webdriver.Firefox(capabilities = caps)
+driver = webdriver.Firefox(capabilities=caps)
 
+#Se carga la pagina y se ubica el chart para que sea visible en el navegador
 driver.get('https://www.transfermarkt.es/jadon-sancho/marktwertverlauf/spieler/401173')
 last_height = driver.execute_script("return document.body.scrollHeight")
-driver.execute_script("window.scrollTo(0,650);")
+driver.execute_script("window.scrollTo(0, 650);")
 time.sleep(3)
 
-chart = driver.find_element_by_css_selector('#highcharts-0')
+#Se ubican las banderas dentro del chart
+chart = driver.find_element_by_css_selector('.highcharts-markers')
 images_found = chart.find_elements_by_tag_name('image')
-wrap_info = driver.find_element_css_selector('div.hightchars-tooltip')
+wrap_info = driver.find_element_by_css_selector('div.highcharts-tooltip')
 
-actions = ActionsChains(driver)
+#Finalmente se ordenara que el cursor pase por cada una de las banderas para ver el tooltip
+#de cada una
+actions = ActionChains(driver)
 info = []
-
 for i in range(len(images_found)):
+    #Moviendo cursor..
     actions.move_to_element(images_found[i])
     actions.perform()
-    print('Recogiendo info...')
+    print("Recogiendo info....")
+    print(images_found[i].text)
     info.append([i.text for i in wrap_info.find_elements_by_tag_name('b')])
